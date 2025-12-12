4829f5676caaf94e84ec6bdc766a3c834799be70
Raya Sesan Firdaus <148180339+rayasesan@users.noreply.github.com>
2025-12-13 00:43:26 +0700

    Update Firebase config to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
 import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 
-// GANTI DENGAN CONFIG KAMU
 const firebaseConfig = {
-  apiKey: "AIzaSyD5OuXA5DW0aW0F3xLDJYWJ03fZmMGxRx4",
-  authDomain: "student-management-26ac6.firebaseapp.com",
-  databaseURL: "https://student-management-26ac6-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "student-management-26ac6",
-  storageBucket: "student-management-26ac6.firebasestorage.app",
-  messagingSenderId: "567433156802",
-  appId: "1:567433156802:web:63f5dd2d6d0d4953dac35b"
-};
-
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+}
 
 const app = initializeApp(firebaseConfig)
 const db = getDatabase(app)
